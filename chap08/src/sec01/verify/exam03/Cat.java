e3161b151f5db8d8c232b7a7d03ca8c0ed64ae15
rkdud <rkdud@DESKTOP-54UOOCU>
2023-11-22 15:34:01 +0900

    chap08 Commit

---
+++ chap08/src/sec01/verify/exam03/Cat.java
@@ -0,0 +1,15 @@
+package sec01.verify.exam03;
+
+public class Cat implements Soundable{
+	
+//	String sound;
+	
+	@Override
+	public String sound() {
+//		this.sound = "야옹";
+//		System.out.println("야옹");
+		return "야옹";
+	}
+	
+
+}
---
+++ chap08/src/sec01/verify/exam03/Dog.java
@@ -0,0 +1,16 @@
+package sec01.verify.exam03;
+
+public class Dog implements Soundable {
+//	String sound;
+	
+//	public Dog (String sound) {
+//		this.sound = sound;
+//	}
+
+	@Override
+	public String sound() {
+//		this.sound = "멍멍";
+//		System.out.println("멍멍");
+		return "멍멍";
+	}
+}
