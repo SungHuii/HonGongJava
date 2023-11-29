001d729ccf2e064d0cb4c078b0c891f5397eeab8
User <User@DESKTOP-R7OCC2A>
2023-11-29 19:09:03 +0900

    chap13 Commit

--- chap13/src/sec01/exam01/ArrayListExample.java
+++ chap13/src/sec01/exam01/ArrayListExample.java
@@ -1,9 +1,41 @@
 package sec01.exam01;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ArrayListExample {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		List<String> list = new ArrayList<String>();
+		
+		list.add("Java"); // String 객체를 저장
+		list.add("JDBC");
+		list.add("Servlet/JSP");
+		list.add(2, "Database");
+		list.add("iBATIS");
+		
+		int size = list.size(); // 저장된 총 객체 수 얻기
+		System.out.println("총 객체 수 : " + size);
+		System.out.println();
+		
+		String skill = list.get(2); // 2번 인덱스의 객체 얻기
+		System.out.println("2 : " + skill);
+		System.out.println();
+		
+		for(int i = 0; i<list.size(); i++) { // 저장된 총 객체 수 만큼 루핑
+			String str = list.get(i);
+			System.out.println(i + " : "+ str );
+		}
+		System.out.println();
+		
+		list.remove(2); // 2번 인덱스 객체(Database) 삭제됨
+		list.remove(2); // 앞의 인덱스가 삭제되어 2번 인덱스가 된 객체(Servlet/JSP) 삭제됨
+		list.remove("iBATIS");
+		
+		for(int i = 0 ; i<list.size(); i++) {
+			String str = list.get(i);
+			System.out.println(i + " : " + str);
+		}
 
 	}
 
